62f7d456f41d386ae9f772a32457400d8e6d59c2
PogadaevEA <18346ggg>
2018-01-23 00:20:57 +0700

    Добавление возможности записи координат в файл в server-core -> ServerController

--- server-core/src/main/java/server/controllers/ServerController.java
+++ server-core/src/main/java/server/controllers/ServerController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 
 
@@ -19,7 +23,19 @@ public class ServerController {
 
     private static Logger log = LoggerFactory.getLogger(ServerController.class);
 
+    private PrintWriter printWriter;
 
+    @PostConstruct
+    private void init(){
+
+        try {
+            printWriter = new PrintWriter("coordinates.txt");
+        }
+        catch(FileNotFoundException e) {
+            e.getMessage();
+        }
+
+    }
 
     @RequestMapping(value = "/coords", method = RequestMethod.POST, consumes = "application/json")
     public Boolean getCoordinates (@RequestBody String jsonCoords){
@@ -27,12 +43,19 @@ public class ServerController {
             ObjectMapper mapper = new ObjectMapper();
             PointDTO coords = mapper.readValue(jsonCoords, PointDTO.class);
             log.info("getCoordinates: lat = " + coords.getLat() + "; lon = " + coords.getLon());
+            printWriter.println(coords.getLon() + "," + coords.getLat());
+            printWriter.flush();
             return true;
         }
         catch (Exception e){
             e.getStackTrace();
             return false;
         }
+    }
+
+    @PreDestroy
+    private void destroy(){
+        printWriter.close();
     }
 
 }
