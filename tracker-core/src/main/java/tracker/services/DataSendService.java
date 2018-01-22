d2237a7e9321d6a883d32cd7f772a35cccfd5a3a
PogadaevEA <18346ggg>
2018-01-23 00:41:25 +0700

    изменение DataSendService отправка -> server-core

--- tracker-core/src/main/java/tracker/services/DataSendService.java
+++ tracker-core/src/main/java/tracker/services/DataSendService.java
@@ -5,8 +5,10 @@ import jdev.dto.PointDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 
 /**
@@ -19,6 +21,9 @@ public class DataSendService {
     @Autowired
     private DataPeekService dataPeekService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
 
     @Scheduled(cron = "${cron.prop.get}")
     private void send () throws InterruptedException{
@@ -28,6 +33,19 @@ public class DataSendService {
         for(int i = 0; i < messageQuantity; i++){
             PointDTO record = dataPeekService.take();
             try {
+                PointDTO pointDTO = new PointDTO();
+                pointDTO.setAutoId(record.getAutoId());
+                pointDTO.setLon(record.getLon());
+                pointDTO.setLat(record.getLat());
+                pointDTO.setAzim(record.getAzim());
+                pointDTO.setSpeed(record.getSpeed());
+                pointDTO.setTime(record.getTime());
+                String request = pointDTO.toJson();
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                HttpEntity<String> coordinates = new HttpEntity<String>(request, headers);
+                ResponseEntity<Boolean> answer = restTemplate.exchange("http://localhost:8080/coords", HttpMethod.POST, coordinates, Boolean.class);
+
                 log.info(record.toJson());
             } catch (JsonProcessingException jpe) {
                 jpe.printStackTrace();
